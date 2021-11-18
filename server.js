ab92c196dfa36a41c5a254aed29ff8e450d07603
CongNV <congnv8192@gmail.com>
2021-11-18 22:01:18 +0700

    .

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function startServer() {
     console.log('connected to db.');
 
     // start listening
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log('listening on port 3000...');
     });
 }
